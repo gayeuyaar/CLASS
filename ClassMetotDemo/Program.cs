28525b6c472cf9123afe903042fe265dd6603d11
gayeuyaar <77752689+gayeuyaar@users.noreply.github.com>
2021-01-24 19:12:06 +0300

    Add project files.

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer müşteri1 = new  Customer
+            {
+                Id = 1 ,
+                Name= " Gaye Nur";
+                Surname = " Uyar ";
+                Address = " Yozgat";
+            }
+
+            Customer müşteri2 = new  Customer
+            {
+                Id = 2 ,
+                Name = " Gönül  ";
+                Surname= "Uyar ";
+                Adres = " Kayseri";
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Müşteri Ekle(müşteri1 );
+            customerManager.Müşteri Ekle(müşteri2 );
+
+            customerManager.ListCustomer(Customer1);
+            customerManager.ListCustomer(customer2);
+
+            customerManager.DelCustomer(Customer1);
+            customerManager.DelCustomer(customer2);
+
+        }
+    }
+}
